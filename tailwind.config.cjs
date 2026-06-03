7fc85f60b741f4292ae433570af233f300912d29
Luca Gazze <lucagazze@MacBook-de-Luca.local>
2026-06-03 17:30:34 -0300

    perf: optimize tailwind css content globs to speed up compilation time (part 4)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./index.html', './**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './{components,pages,services,contexts,hooks}/**/*.{js,ts,jsx,tsx}',
+    './App.tsx',
+    './index.tsx'
+  ],
   theme: {
     extend: {},
   },
